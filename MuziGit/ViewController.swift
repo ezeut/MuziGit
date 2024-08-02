df002dc08fce5a5d38575147c8a4651ba2afc2f6
ezeut <ezeut@naver.com>
2024-08-03 01:12:13 +0900

    온보딩 PageView 생성

--- MuziGit/ViewController.swift
+++ MuziGit/ViewController.swift
@@ -8,12 +8,57 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var onboardingPageControl: UIPageControl!
+    var onboardingPageViewController: OnboardingPageViewController!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        onboardingPageControl.preferredIndicatorImage = UIImage(named: "defaultPageIndicator")
+        onboardingPageControl.setIndicatorImage(UIImage(named: "currentPageIndicator"), forPage: 0)
+        //            onboardingPageControl.pageIndicatorTintColor = UIColor(named: "Gray")
+        //            onboardingPageControl.currentPageIndicatorTintColor = UIColor(named: "Neo")
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let desinationViewController = segue.destination as? OnboardingPageViewController {
+            onboardingPageViewController = desinationViewController
+            onboardingPageViewController.onboardingDelegate = self
+        }
+    }
+    
+    @IBAction func changePageValue(_ sender: UIPageControl) {
+        let currentPageIndex = onboardingPageControl.currentPage
+        onboardingPageViewController.movePage(index: currentPageIndex)
+        updatePageControl(currentPageIndex: currentPageIndex)
     }
 
-
+    func updatePageControl(currentPageIndex: Int) {
+        //            onboardingPageControl.pageIndicatorTintColor = UIColor(named: "Gray")
+        //            onboardingPageControl.currentPageIndicatorTintColor = UIColor(named: "Neo")
+        
+        (0..<onboardingPageControl.numberOfPages).forEach { (index) in
+            let currentPageIconImage = UIImage(named: "currentPageIndicator")
+            let defaultPageIconImage = UIImage(named: "defaultPageIndicator")
+            let pageIcon = index == currentPageIndex ? currentPageIconImage : defaultPageIconImage
+            onboardingPageControl.setIndicatorImage(pageIcon, forPage: index)
+        }
+    }
 }
 
+extension ViewController: OnboardingPageControlDelegate {
+    func numberOfPage(numberOfPage: Int) {
+        onboardingPageControl.numberOfPages = numberOfPage
+    }
+    
+    func pageChangedTo(index: Int) {
+        updatePageControl(currentPageIndex: index)
+        onboardingPageControl.currentPage = index
+    }
+}
+
+protocol OnboardingPageControlDelegate: AnyObject {
+    func numberOfPage(numberOfPage: Int)
+    func pageChangedTo(index: Int)
+}
